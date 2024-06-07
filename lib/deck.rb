15d641a796dc533e29fe6e28b13fac5ddec221bb
Jullian Calkins <jullian0426@yahoo.com>
2024-06-07 11:20:09 -0400

    Create shuffle function for deck

--- lib/deck.rb
+++ lib/deck.rb
@@ -21,4 +21,8 @@ class Deck
   def deal
     cards.pop
   end
+
+  def shuffle(seed = Random.new)
+    cards.shuffle!(random: seed)
+  end
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -27,10 +27,18 @@ RSpec.describe Deck do
   end
 
   describe '#deal' do
-    it 'should remove the top card from the deck' do
+    it 'should remove the top card' do
       dealt_card = deck.deal
       expect(dealt_card).to respond_to :rank
       expect(deck_size).to eq 51
+    end
+  end
+
+  describe '#shuffle' do
+    it 'randomized the order of cards' do
+      original_cards = deck.cards.dup
+      deck.shuffle(Random.new(1))
+      expect(deck.cards).to_not eq original_cards
     end
   end
 end
